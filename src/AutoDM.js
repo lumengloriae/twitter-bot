e8a00745abba8239b1254e50754c0b89fe9486eb
lumengloriae <46624508+lumengloriae@users.noreply.github.com>
2019-01-12 14:31:58 +0200

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Здравствуйте! Спасибо Вам за подписку! Для того, чтобы Вы могли извлечь максимальную пользу, я рекомендую Вам почитать мои статьи в Яндекс.Дзен. Ссылка на Дзен https://zen.yandex.ru/id/5c34b4138655d100a95f8889`; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
